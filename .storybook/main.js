135a57573c838e7a853092de2fefa4a6811747c7
Alexander Gurnet <Alexandergurnet@gmail.com>
2022-05-09 18:59:54 +0300

    added storybook and downgraded some dependencies

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        cssLoaderOptions: {
+          modules: { localIdentName: '[name]__[local]--[hash:base64:5]' },
+        },
+      },
+    },
+  ],
+  framework: '@storybook/react',
+};
